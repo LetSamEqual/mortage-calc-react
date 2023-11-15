102f468e5fe3431df4523005636e7c06f7c37318
LetSamEqual <samahenderson24@gmail.com>
2023-11-15 16:28:12 +1100

    added sitemap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import { Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import NavBar from "./components/navBar/navBar";
 import LandingPage from "./components/landingPage/landingPage";
 import LinksPage from "./components/linksPage/linksPage";
@@ -9,14 +10,16 @@ import AboutPage from "./components/aboutPage/aboutPage";
 function App() {
   return (
     <div className="App">
-      <NavBar className="navbar" />
-      <div className="contentContainer">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/resources" element={<LinksPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </div>
+      <HelmetProvider>
+        <NavBar className="navbar" />
+        <div className="contentContainer">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/resources" element={<LinksPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
+      </HelmetProvider>
     </div>
   );
 }

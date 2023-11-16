e5f39733a9393ff17a725da70a16ddc67d1c4a90
LetSamEqual <samahenderson24@gmail.com>
2023-11-16 16:27:58 +1100

    added rich snippets and structured data for google searches

--- src/components/siteIntro/siteIntro.js
+++ src/components/siteIntro/siteIntro.js
@@ -4,22 +4,30 @@ import "./siteIntro.css";
 const SiteIntro = () => {
   return (
     <div className="introContainer">
-      <h2 className="introContainerHeading">
+      <h1 className="introContainerHeading">
         Do the hard work of planning for the future,
         <br /> minus the hard work
-      </h2>
+      </h1>
       <p className="introParaText">
-        MortgageBudget.com.au was designed to do away with the complicated
-        spreadsheets and formulas that go into buying a home, so you can get a
-        clear picture of your finances, without the hassle. Fill in your
-        mortgage budget below to see what property you can afford.
+        Our <span>mortgage calculator</span> was designed to do away with the
+        complicated spreadsheets and formulas that go into buying a home, so you
+        can get a <span>clear picture of your finances</span>, without the
+        hassle.{" "}
+        <span>
+          Add your personal budget to the mortgage calculator below to see what
+          property you can afford.
+        </span>
       </p>
       <p className="introParaText">
         This site was developed by the people who know what goes into purchasing
-        property, from conveyancers to real estate agents to real people trying
-        to buy their first home. The result is a mortgage calculator that asks
-        the right questions and gives the right answers, so you can step into
-        the property market with confidence.
+        property, from{" "}
+        <span>
+          conveyancers to real estate agents to real people trying to buy their
+          first home
+        </span>
+        . The result is a mortgage calculator that asks the right questions and
+        gives the right answers, so you can step into the property market
+        with confidence.
       </p>
       <p className="introParaText">
         From the team at MortgageBudget.com.au, good luck and happy
